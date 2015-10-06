78a2ef2c8eac0d7876c1a8f67f36f009ae2722af
Felix <martinez.alvaro.felix@gmail.com>
2015-10-06 13:58:15 +0200

    Workers

---
+++ WebWorkers/funcionesIndex.js
@@ -0,0 +1,43 @@
+﻿var worker;
+
+function lanzarWorker() {
+    worker = new Worker("funcionesWebWorker.js");
+    worker.onmessage = function (evt) {
+
+        var array = evt.data.split(":");
+        document.getElementById("horas").innerHTML = array[0];
+        document.getElementById("minutos").innerHTML = array[1];
+        document.getElementById("segundos").innerHTML = array[2];
+
+        //if (evt.data == "0") {
+        //    location.reload();
+        //}
+
+    }
+}
+
+
+
+function parar() {
+
+    worker.terminate();
+    worker = undefined;
+
+}
+
+
+function reiniciar() {
+
+
+    if (worker == undefined)
+        lanzarWorker();
+    else
+        worker.postMessage("reiniciar");
+
+}
+
+lanzarWorker();
+document.getElementById("reiniciar").addEventListener("click", reiniciar);
+document.getElementById("stop").addEventListener("click", parar);
+
+
---
+++ WebWorkers/funcionesWebWorker.js
@@ -0,0 +1,35 @@
+﻿var horas = 0;
+var minutos = 0;
+var segundos = 0;
+
+function bajar() {
+
+    segundos++;
+
+    if (segundos == 60) {
+        minutos++;
+        segundos = 0;
+        if (minutos == 60) {
+            horas++;
+            minutos = 0;
+            segundos = 0;
+        }
+    }
+
+    
+
+    self.postMessage(horas+":"+minutos+":"+segundos);
+    setTimeout("bajar()", 1000);
+
+}
+
+self.onmessage = function(evt) {
+
+    if (evt.data == "reiniciar")
+        segundos = 0;
+        minutos = 0;
+        horas = 0;
+}
+
+bajar();
+
